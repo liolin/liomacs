65b0847b0fefb476f6a7f82d985053570bad9e32
Olivier Lischer <olivier.lischer@liolin.ch>
2021-12-20 12:47:04 +0100

    Add lsp-ltex for spell checking.

--- modules/liomacs-diagnostics.el
+++ modules/liomacs-diagnostics.el
@@ -1,2 +1,11 @@
 (use-package flycheck)
+
+(use-package lsp-ltex
+  :ensure t
+  :config
+  (flycheck-add-next-checker 'lsp 'proselint)
+  :hook (text-mode . (lambda ()
+                       (require 'lsp-ltex)
+                       (lsp))))
+
 (provide 'liomacs-diagnostics)
