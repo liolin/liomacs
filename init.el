841d15a23ad3faab8f3658c75e943f3a7d882948
Olivier Lischer <olivier.lischer@liolin.ch>
2022-02-05 14:06:06 +0100

    Add kotlin, csharp

--- init.el
+++ init.el
@@ -42,6 +42,8 @@
     liomacs-debugging
     liomacs-rust
     liomacs-cpp
+    liomacs-csharp
+    liomacs-kotlin
     liomacs-mail
     liomacs-server)
   "A list of all modules to load")
@@ -60,3 +62,22 @@
 
 
 ;; End init.el
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((eval add-hook 'after-save-hook
+	   (lambda nil
+	     (if
+		 (y-or-n-p "Tangle?")
+		 (org-babel-tangle)))
+	   nil t)
+     (org-attach-id-dir . "attachment/"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
