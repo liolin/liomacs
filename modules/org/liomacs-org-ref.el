b9bda67d8662df7a9d08f5fdc5211b6f95ef5670
Olivier Lischer <olivier.lischer@liolin.ch>
2022-04-07 11:43:55 +0200

    Add org-ref setup

---
+++ modules/org/liomacs-org-ref.el
@@ -0,0 +1,10 @@
+(use-package org-ref
+  :bind (:map org-mode-map
+	      ("C-c ]" . org-ref-insert-link))
+  :config
+  (setq org-latex-prefer-user-labels t))
+
+
+
+;(define-key org-mode-map (kbd "C-c ]") 'org-ref-insert-link)
+;(setq org-latex-prefer-user-labels t)
