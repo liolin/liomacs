d68cf46dfbc8e01dff7ca14b02edba3dd54e8b8d
Olivier Lischer <olivier.lischer@liolin.ch>
2023-01-10 15:59:55 +0100

    Change default shell inside emacs to fish

--- modules/liomacs-shell.el
+++ modules/liomacs-shell.el
@@ -1,6 +1,8 @@
 (use-package better-shell
     :ensure t
     :bind (("C-'" . better-shell-shell)
-           ("C-;" . better-shell-remote-open)))
+           ("C-;" . better-shell-remote-open))
+    :config
+    (setq shell-file-name "fish"))
 
 (provide 'liomacs-shell)
