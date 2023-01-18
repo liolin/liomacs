e12e0654ea9f54d81fab9d0526c4e4f5c7ae84c1
Olivier Lischer <olivier.lischer@liolin.ch>
2023-01-18 08:23:54 +0100

    Add citation support in org mode

--- modules/org/liomacs-org-ref.el
+++ modules/org/liomacs-org-ref.el
@@ -4,7 +4,17 @@
   :config
   (setq org-latex-prefer-user-labels t))
 
-
-
-;(define-key org-mode-map (kbd "C-c ]") 'org-ref-insert-link)
-;(setq org-latex-prefer-user-labels t)
+(use-package citar)
+(use-package org
+  :custom
+  (org-cite-global-bibliography '("~/biblio/main.bib"))
+  (org-cite-insert-processor 'citar)
+  (org-cite-follow-processor 'citar)
+  (org-cite-activate-processor 'citar)
+  (citar-bibliography '("~/biblio/main.bib"))
+  (citar-notes-paths '("~/biblio/main"))
+  (citar-symbols
+   `((file ,(all-the-icons-faicon "file-pdf-o" :face 'all-the-icons-green :v-adjust -0.1) . " ")
+     (note ,(all-the-icons-material "speaker_notes" :face 'all-the-icons-blue :v-adjust -0.3) . " ")
+     (link ,(all-the-icons-octicon "link" :face 'all-the-icons-orange :v-adjust 0.01) . " ")))
+  (citar-symbol-separator "  "))
