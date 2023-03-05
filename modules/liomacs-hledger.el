5fe3c9f4627641d0d2f82164ff3268560f0e97a3
Olivier Lischer <olivier.lischer@liolin.ch>
2023-03-05 14:39:50 +0100

    Change hledger with ledger

--- modules/liomacs-hledger.el
+++ modules/liomacs-hledger.el
@@ -1,16 +1,25 @@
-(defun liomacs/hledger-mode-setup ()
-  (company-mode)
-  (make-local-variable 'company-backends)
-  (add-to-list 'company-backends 'hledger-company))
+;; (defun liomacs/hledger-mode-setup ()
+;;   (company-mode)
+;;   (make-local-variable 'company-backends)
+;;   (add-to-list 'company-backends 'hledger-company))
 
-(use-package hledger-mode
-  :bind (("C-c e" . hledger-jentry)
-	 ("C-c j" . hledger-run-comand))
-  :hook
-  (hledger-mode . liomacs/hledger-mode-setup)
+;; (use-package hledger-mode
+;;   :bind (("C-c e" . hledger-jentry)
+;; 	 ("C-c j" . hledger-run-comand))
+;;   :hook
+;;   (hledger-mode . liomacs/hledger-mode-setup)
+;;   :custom
+;;   (hledger-jfile "/home/liolin/org/finance/current.journal")
+;;   (hledger-currency-string "CHF")
+;;   :init
+;;   (add-to-list 'auto-mode-alist '("\\.journal\\'" . hledger-mode)))
+
+
+(use-package ledger-mode
   :custom
-  (hledger-jfile "/home/liolin/finance/2021.journal")
-  (hledger-currency-string "CHF")
+  (ledger-mode-should-check-version nil)
+  (ledger-report-links-in-register nil)
+  (ledger-binary-path "hledger")
   :init
   (add-to-list 'auto-mode-alist '("\\.journal\\'" . hledger-mode)))
 
