19ae826c138b1ca407a22f479f6b9419a380e198
Olivier Lischer <olivier.lischer@liolin.ch>
2025-08-23 10:16:11 +0200

    Replace icomplete with vertico

--- init.el
+++ init.el
@@ -219,30 +219,44 @@
 	 )))
 
 ;;; ICOMPLETE
-(use-package icomplete
-  :bind (:map icomplete-minibuffer-map
-			  ("C-n" . icomplete-forward-completions)
-			  ("C-p" . icomplete-backward-completions)
-			  ("C-v" . icomplete-vertical-toggle)
-			  ("RET" . icomplete-force-complete-and-exit)
-			  ("C-j" . exit-minibuffer)) ;; So we can exit commands like `multi-file-replace-regexp-as-diff'
-  :hook
-  (after-init . (lambda ()
-				  (fido-mode -1)
-				  (icomplete-vertical-mode 1)))
+;; (use-package icomplete
+;;   :bind (:map icomplete-minibuffer-map
+;;			  ("C-n" . icomplete-forward-completions)
+;;			  ("C-p" . icomplete-backward-completions)
+;;			  ("RET" . icomplete-force-complete-and-exit)
+;;			  ("C-j" . exit-minibuffer)) ;; So we can exit commands like `multi-file-replace-regexp-as-diff'
+;;   :hook
+;;   (after-init . (lambda ()
+;;				  (fido-mode -1)
+;;				  (icomplete-vertical-mode 1)))
+;;   :config
+;;   (setq icomplete-delay-completions-threshold 0)
+;;   (setq icomplete-compute-delay 0)
+;;   (setq icomplete-show-matches-on-no-input t)
+;;   (setq icomplete-hide-common-prefix nil)
+;;   (setq icomplete-prospects-height 10)
+;;   (setq icomplete-separator " . ")
+;;   (setq icomplete-with-completion-tables t)
+;;   (setq icomplete-in-buffer nil)
+;;   (setq icomplete-max-delay-chars 0)
+;;   (setq icomplete-scroll t)
+;;   (advice-add 'completion-at-point
+;;			  :after #'minibuffer-hide-completions))
+
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode)
+  :bind
+  (:map vertico-map
+		("C-j" . vertico-next)
+		("C-k" . vertico-previous))
+  :custom
+  (vertico-cycle t)
   :config
-  (setq icomplete-delay-completions-threshold 0)
-  (setq icomplete-compute-delay 0)
-  (setq icomplete-show-matches-on-no-input t)
-  (setq icomplete-hide-common-prefix nil)
-  (setq icomplete-prospects-height 10)
-  (setq icomplete-separator " . ")
-  (setq icomplete-with-completion-tables t)
-  (setq icomplete-in-buffer nil)
-  (setq icomplete-max-delay-chars 0)
-  (setq icomplete-scroll t)
-  (advice-add 'completion-at-point
-			  :after #'minibuffer-hide-completions))
+  (setq read-file-name-completion-ignore-case t
+		read-buffer-completion-ignore-case t
+		completion-ignore-case t))
 
 (use-package imenu
   :ensure nil
