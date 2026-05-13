49b102518b146ff91ac45eb159499c2b743210ab
Olivier Lischer <olivier.lischer@liolin.ch>
2026-05-13 14:53:35 +0200

    Add xenops to render LaTeX math

--- init.el
+++ init.el
@@ -1389,6 +1389,35 @@ and restart Flymake to apply the changes."
         TeX-save-query nil
         TeX-source-correlate-method 'synctex))
 
+(use-package xenops
+  :ensure t
+  :defer t
+  :config
+  (setq xenops-math-latex-process-alist
+        '((dvipng :programs ("latex" "dvipng") :description "dvi > png"
+                  :message
+                  "you need to install the programs: latex and dvipng."
+                  :image-input-type "dvi" :image-output-type "png"
+                  :image-size-adjust (1.0 . 1.0) :latex-compiler
+                  ("lualatex -interaction nonstopmode -shell-escape -output-format dvi -output-directory %o %f")
+                  :image-converter ("dvipng -D %D -T tight -o %O %f"))
+          (dvisvgm :programs ("latex" "dvisvgm") :description "dvi > svg"
+                   :message
+                   "you need to install the programs: latex and dvisvgm."
+                   :image-input-type "dvi" :image-output-type "svg"
+                   :image-size-adjust (1.7 . 1.5) :latex-compiler
+                   ("lualatex -interaction nonstopmode -shell-escape -output-format dvi -output-directory %o %f")
+                   :image-converter ("dvisvgm %f -n -b %B -c %S -o %O"))
+          (imagemagick :programs ("latex" "convert") :description "pdf > png"
+                       :message
+                       "you need to install the programs: latex and imagemagick."
+                       :image-input-type "pdf" :image-output-type "png"
+                       :image-size-adjust (1.0 . 1.0) :latex-compiler
+                       ("pdflatex -interaction nonstopmode -shell-escape -output-directory %o %f")
+                       :image-converter
+                       ("convert -density %D -trim -antialias %f -quality 100 %O")))))
+
+
 (use-package eglot-ltex-plus
   :init
   (setq eglot-ltex-plus-server-path (expand-file-name "ltex-ls-plus/" user-emacs-directory)
